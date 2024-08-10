316618ab76a0cb496937241213757940de74f48b
lachlancrabbJCU <140355342+lachlancrabbJCU@users.noreply.github.com>
2024-08-10 22:03:19 +1000

    Refactor display messages

--- app.py
+++ app.py
@@ -5,38 +5,46 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
+    """Display Hello World with HTML formatting."""
     return '<h1>Hello World :)</h1>'
 
 
 @app.route('/greet')
 @app.route('/greet/<name>')
 def greet(name=""):
+    """Display greeting to name in url."""
     return f"Hello {name}"
 
 
 @app.route('/f/<celsius>')
 def display_fahrenheit(celsius):
-    if validate_float(celsius):
-        return f"{convert_celsius_fahrenheit(float(celsius))}"
+    """Display converted Fahrenheit from Celsius in url."""
+    celsius = validate_float(celsius)
+    if celsius:
+        fahrenheit = convert_celsius_fahrenheit(celsius)
+        return f"{celsius} C is {fahrenheit} F"
     else:
-        return "Invalid input e.g. /f/10.0"
+        return "Invalid input e.g. url/f/10.0"
 
 
 @app.route('/c/<fahrenheit>')
 def display_celsius(fahrenheit):
-    if validate_float(fahrenheit):
-        return f"{convert_fahrenheit_celsius(float(fahrenheit))}"
+    """Display converted Celsius from Fahrenheit in url."""
+    fahrenheit = validate_float(fahrenheit)
+    if fahrenheit:
+        celsius = convert_fahrenheit_celsius(fahrenheit)
+        return f"{fahrenheit} F is {celsius} C"
     else:
-        return "Invalid input e.g. /c/10.0"
+        return "Invalid input e.g. url/c/10.0"
 
 
 def validate_float(string):
     """Validate string can be cast to float type."""
     try:
-        float(string)
-        return True
+        valid_float = float(string)
+        return valid_float
     except ValueError:
-        return False
+        return None
 
 
 def convert_fahrenheit_celsius(fahrenheit):
